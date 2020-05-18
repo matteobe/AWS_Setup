f9f28b89dc8c1720cf1ad2bd4447012c24048d73
Matteo <maberchier@gmail.com>
2020-05-18 16:28:03 +0200

    Update jupiter_setup.sh

--- jupiter_setup.sh
+++ jupiter_setup.sh
@@ -4,5 +4,30 @@
 wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
 
 # Launch Anaconda installer
-yes | bash Anaconda3-2020.02-Linux-x86_64.sh
+bash Anaconda3-2020.02-Linux-x86_64.sh
 
+# Check that the PATH variable contains the Anaconda environment
+SUB='anaconda3/bin'
+if [ ! grep -q "$SUB" <<< "$PATH"]; then
+  export PATH=/home/ubuntu/anaconda3/bin:$PATH
+fi
+
+# Check that Anaconda is the preferred environment
+PENV='which python'
+if [ grep -q "$SUB" <<< "$PENV"]; then
+  echo "Anaconda is the current Python environment"
+fi
+
+# Configure Jupiter Notebooks
+jupyter notebook --generate-config
+
+# Generate certificates for HTTPS
+mkdir certs
+cd certs
+openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout mycert.pem -out mycert.pem
+
+# Configure Jupiter
+cd ~/.jupiter/
+SUB="c = get_config()\n\
+
+sed -i '1,xs/^/c = get_config()\n/' jupyter_notebook_config.py
