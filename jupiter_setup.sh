2a4c747c1862993af9e41a1aff48a43da1783024
Matteo <maberchier@gmail.com>
2020-05-18 16:03:20 +0200

    Create jupiter_setup.sh

---
+++ jupiter_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Download current Anaconda version
+wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
+
+# Launch Anaconda installer
+yes | bash Anaconda3-2020.02-Linux-x86_64.sh
+
